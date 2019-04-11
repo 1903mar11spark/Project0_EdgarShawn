af35fafd202482fda364b24095d34cdbad3e3102
Shawn Currey <shawn.m.currey@gmail.com>
2019-04-11 16:22:33 -0400

    pushing Function screen and register screen, edited homescreen

--- BankingApp/BankingApp/src/main/java/com/revature/beans/Customer.java
+++ BankingApp/BankingApp/src/main/java/com/revature/beans/Customer.java
@@ -48,7 +48,7 @@ public class Customer
 		@Override
 		public String toString() {
 			return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", userName="
-					+ userName + ", passWord=" + passWord + "]";
+					+ userName + ", passWord=" + passWord + ", role=" + role + "]";
 		}
 
 		
