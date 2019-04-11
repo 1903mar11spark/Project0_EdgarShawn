64165416056514ceb704f7e37de79c5896c7fa08
Edgar Padilla <MainComp@USF-Gold-Wifi-226-109-100.laptops.usf.edu>
2019-04-11 13:41:41 -0400

    pushing to master

--- BankingApp/BankingApp/src/main/java/com/revature/dao/CustomerDAOImpl.java
+++ BankingApp/BankingApp/src/main/java/com/revature/dao/CustomerDAOImpl.java
@@ -1,8 +1,14 @@
 package com.revature.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import com.revature.beans.Account;
 import com.revature.beans.Customer;
+import com.revature.util.ConnectionUtil;
 
 public class CustomerDAOImpl implements CustomerDAO{
 
@@ -50,7 +56,35 @@ public class CustomerDAOImpl implements CustomerDAO{
 
 	@Override
 	public Customer getCustomerByCredentials(String username, String password) {
-		// TODO Auto-generated method stub
+		Account account = null;
+		
+		try(Connection con = ConnectionUtil.getConnection())
+		{
+			String sql = "SELECT ACCOUNT_ID, ACCOUNT_NUMBER, BALANCE FROM ACCOUNTS WHERE USERNAME = ? AND"
+					+ " PASS_word = ?";
+			
+			PreparedStatement pstmt  = con.prepareStatement(sql);
+			
+			//Set value of the first '?' to the value of 'id' 
+			pstmt.setString(1, username);
+			pstmt.setString(2,password);
+			
+			//Result the query and retrieve a ResultSet
+			ResultSet rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				int accountId = rs.getInt("ACCOUNT_ID");
+				int accountNum = rs.getInt("ACCOUNT_NUMBER");
+				double balance = rs.getDouble("BALANCE");
+				account = new Account(accountId, accountNum, balance);
+			}
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+
+		
+		return account;
 		return null;
 	}
 
